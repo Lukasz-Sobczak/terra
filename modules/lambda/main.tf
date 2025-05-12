dbb13c6872e3c5cde86c2364d270caf15de962ce
Lukasz-Sobczak <263457@student.pwr.edu.pl>
2025-05-12 16:40:31 +0200

    Mozezadzialawreszcie

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "sensor_function" {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda/lambda"
+  source_dir = "${path.module}/lambda"
   output_path = "${path.module}/lambda.zip"
 }
 
