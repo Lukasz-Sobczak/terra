48b6f326c00c287c87b209e0ba11ec3e2e260b16
Lukasz-Sobczak <263457@student.pwr.edu.pl>
2025-05-12 16:10:11 +0200

    Moduleszip

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "sensor_function" {
   function_name    = var.function_name
   role             = var.lambda_role_arn
   handler          = var.handler
-  source_code_hash = filebase64sha256(var.lambda_zip)
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.12"
   timeout          = 10
 }
