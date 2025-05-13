ced6a0f683d7ee3103bca92faefadea3e36c6050
Lukasz-Sobczak <263457@student.pwr.edu.pl>
2025-05-13 12:23:31 +0200

    lij

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -54,7 +54,7 @@ def lambda_handler(event, context):
         status = {"status": "TEMPERATURE TOO LOW"}
     elif T_celsius < 100:
         status = {"status": "OK"}
-    elif T_celsius < 250:
+    elif T_celsius < 240:
         status = {"status": "TEMPERATURE TOO HIGH"}
     else:
         status = {"status": "TEMPERATURE CRITICAL"}
